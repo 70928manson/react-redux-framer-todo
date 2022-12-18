0034d28f716f7c599671ead7d2db6d674474ab0e
Manson <manson972@yahoo.com>
2022-12-18 15:56:00 +0800

    project setting init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+
+function App() {
+  return (
+    <div className="container">
+      <PageTitle></PageTitle>
+    </div>
+  );
+}
+
+export default App;
