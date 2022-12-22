8959e51d0150cb7082c21e4d54db875ee96d2243
Manson <manson972@yahoo.com>
2022-12-23 00:59:05 +0800

    todo add modal update

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
+import TodoModal from './TodoModal';
 import styles from '../styles/modules/app.module.scss';
 
 const AppHeader = () => {
+    const [modalOpen, setModalOpen] = useState(false);
     return (
         <div className={styles.appHeader}>
-            <Button variant="primary">Click me</Button>
+            <Button variant="primary" onClick={() => setModalOpen(true)}>Add Task</Button>
             <SelectButton id="status">
                 <option value="all">ALL</option>
                 <option value="incomplete">incomplete</option>
                 <option value="complete">complete</option>
             </SelectButton>
+            <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen}></TodoModal>
         </div>
     );
 };
